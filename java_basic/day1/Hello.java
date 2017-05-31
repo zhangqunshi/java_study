90d232a40380b07afe033b581197c658acf95313
zhangqunshi <zhangqunshi@126.com>
2017-05-31 23:38:28 +0800

    First day java code

---
+++ java_basic/day1/Hello.java
@@ -0,0 +1,16 @@
+/**
+ 这是注释，不会被运行
+ ------------
+ 这是第一个JAVA程序，步骤是：
+ 1) 下载Java SDK, 在系统环境变量中配置JAVA_HOME
+ 2) 编译代码: javac Hello.java
+ 3) 运行代码: java Hello
+ 
+ 注意： Java区分大小写
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hello, Kris");
+    }
+}
