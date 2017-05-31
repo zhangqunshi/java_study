520f5358cf2aa1aefc27dd02cd3620ed0c1545c4
zhangqunshi <zhangqunshi@126.com>
2017-06-01 00:27:14 +0800

    Get database connection example

---
+++ jdbc/DatabaseConnection.java
@@ -0,0 +1,37 @@
+import java.sql.*;
+
+public class DatabaseConnection {
+
+    final static String DB_NAME = "test"; // you need create a database called test
+    final static String DB_USER = "root";
+    final static String DB_PASSWORD = "123456";
+
+    /**
+     * Get database connection
+     * This example use MySQL database, so you need download mysql first,
+     * and download the mysql driver jar file, add it to your classpath.
+     * <p>
+     * 注意：运行此文件需要mysql的驱动jar，并且把jar配置到classpath中, 在windows系统中
+     * classpath的配置如下：set classpath=.;mysql-connector-java-5.0.7-bin.jar
+     * 如果DOS下无法运行，请把这段中文注释删除，可能是GBK编码问题。
+     */
+    public Connection getConnection() {
+        Connection conn = null;
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/" + DB_NAME, DB_USER, DB_PASSWORD);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return conn;
+    }
+
+    public static void main(String[] args) throws SQLException {
+        DatabaseConnection d = new DatabaseConnection();
+        Connection conn = d.getConnection();
+        System.out.println("Get database connection: " + conn);
+        conn.close();
+    }
+}
