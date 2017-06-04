2ca89177e19d7be345b1dd1d656851d5bb52309e
zhangqunshi <zhangqunshi@126.com>
2017-06-04 15:47:32 +0800

    add StringEquals.java

---
+++ java_basic/day2_variable_operator/StringEquals.java
@@ -0,0 +1,13 @@
+class StringEquals {
+    public static void main(String[] args) {
+        String a = "a";
+        String b = "b";
+
+        if (a.equals(b)) {
+            System.out.println("a = b");
+        } else {
+            System.out.println("a != b");
+        }
+
+    }
+}
