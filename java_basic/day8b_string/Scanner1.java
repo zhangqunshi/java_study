9311233b926e8f66d810aca7a6606e831e377994
zhangqunshi <zhangqunshi@126.com>
2017-07-20 21:32:37 +0800

    Scanner example

---
+++ java_basic/day8b_string/Scanner1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Scanner1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String x = sc.nextLine();
+        System.out.println(x);
+        sc.close();
+    }
+}
---
+++ java_basic/day8b_string/Split1.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Split1 {
+    public static void main(String[] args) {
+        String x = "hello kris how are you";
+        String[] y = x.split(" ");
+        System.out.println(Arrays.toString(y));
+    }
+}
