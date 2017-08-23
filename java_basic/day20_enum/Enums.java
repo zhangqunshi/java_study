d5527e35d3ae0cb22cb9e8bef4eb18af762d9d05
zhangqunshi <zhangqunshi@126.com>
2017-08-23 22:47:43 +0800

    java_basic\day20_enum\Enums.java

---
+++ java_basic/day20_enum/Enums.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Enums {
+    private static Random rand = new Random(47);
+
+    public static <T extends Enum<T>> T random(Class<T> c) {
+        return random(c.getEnumConstants());
+    }
+
+    public static <T> T random(T[] values) {
+        return values[rand.nextInt(values.length)];
+    }
+}
