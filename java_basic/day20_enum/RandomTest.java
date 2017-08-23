301b71b8e418d236bfaa3722633507f8cdc0bb21
zhangqunshi <zhangqunshi@126.com>
2017-08-23 22:49:00 +0800

    java_basic\day20_enum\RandomTest.java

---
+++ java_basic/day20_enum/RandomTest.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+enum Activity {
+    A, B, C, D,
+}
+
+public class RandomTest {
+    public static void main(String[] args) {
+        for (int i = 0; i < 20; i++) {
+            System.out.println(Enums.random(Activity.class));
+        }
+    }
+}
