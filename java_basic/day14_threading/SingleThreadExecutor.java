a237a142530b9aff1abe0115e56617e1f1ec4f60
zhangqunshi <zhangqunshi@126.com>
2017-08-31 18:02:49 +0800

    java_basic\day14_threading\SingleThreadExecutor.java

---
+++ java_basic/day14_threading/SingleThreadExecutor.java
@@ -0,0 +1,13 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class SingleThreadExecutor {
+    public static void main(String[] args) {
+        ExecutorService exec = Executors.newSingleThreadExecutor();
+        for (int i = 0; i < 5; i++) {
+            exec.execute(new LiftOff());
+        }
+
+        exec.shutdown();
+    }
+}
