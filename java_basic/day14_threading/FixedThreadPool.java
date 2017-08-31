b44e9aeec7391fabfe0db2712f2ac5ae2bf63ed7
zhangqunshi <zhangqunshi@126.com>
2017-08-31 18:02:21 +0800

    java_basic\day14_threading\FixedThreadPool.java

---
+++ java_basic/day14_threading/FixedThreadPool.java
@@ -0,0 +1,13 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class FixedThreadPool {
+    public static void main(String[] args) {
+        ExecutorService exec = Executors.newFixedThreadPool(5);
+        for (int i = 0; i < 5; i++) {
+            exec.execute(new LiftOff());
+        }
+
+        exec.shutdown();
+    }
+}
